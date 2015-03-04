c56f8792e43213faf14118469cd8b66e85a43b4c
Kevin Gabinete <kgabinete08@gmail.com>
2015-03-03 21:14:13 -0800

    work on the users controller and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password validations: false
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
+  get 'login', to: 'users#index'
+  get '/logout', to: 'users#destroy' 
+
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
+  resources :users, only: [:create]
 end

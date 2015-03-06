494b54d650ec717a871f3c6fe42c7c26ed88d352
Kevin Gabinete <kgabinete08@gmail.com>
2015-03-05 20:35:33 -0800

    finished authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,18 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    if !logged_in?
+      flash[:error] = "Please log in first."
+      redirect_to root_path
+    end
+  end
+
 end

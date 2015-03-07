5a3c1dd143bfff6c57431d2c384abf84782f2c67
Kevin Gabinete <kgabinete08@gmail.com>
2015-03-06 21:47:07 -0800

    work on user show and edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show]
+
   def new
     @user = User.new
   end
@@ -8,15 +10,24 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:notice] = "Your account was created successfully."
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
     end
   end
 
+  def edit; end
+
+  def show; end
+
   private
 
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
-  validates :password, confirmation: true, on: :create
+  validates :password, confirmation: true, on: :create, length: {minimum: 5}
   validates :password_confirmation, presence: true
 end

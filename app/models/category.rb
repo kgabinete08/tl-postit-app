fce92b945854d83c4a470ddea76113c2774505b2
Kevin Gabinete <kgabinete08@gmail.com>
2015-03-19 21:31:30 -0700

    improved slugs for post, user and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,6 +10,29 @@ class Category < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.name.gsub(" ", "-").downcase
+    the_slug = to_slug(self.title)
+    category = Category.find_by slug: the_slug
+    count = 2
+    while category && category != self
+      the_slug = append_suffix(the_slug, count)
+      category = Category.find_by slug: the_slug
+      count += 1
+    end
+    self.slug = the_slug.downcase
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + "-" + count.to_s
+    else
+      return str + '-' + count.to_s
+    end
+  end
+
+  def to_slug(name)
+    str = name.strip
+    str.gsub! /\W/, "-"
+    str.gsub! /-+/, "-"
+    str.downcase
   end
 end

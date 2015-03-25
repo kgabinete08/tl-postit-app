bb9060131e58cb15cd9e327eb821ae0282305452
Kevin Gabinete <kgabinete08@gmail.com>
2015-03-24 21:45:10 -0700

    removed dependency on voteable gem

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,19 +1,19 @@
-# module Voteable
-#   extend ActiveSupport::Concern
+module Voteable
+  extend ActiveSupport::Concern
 
-#   included do
-#     has_many :votes, as: :voteable
-#   end
+  included do
+    has_many :votes, as: :voteable
+  end
 
-#   def upvotes
-#     self.votes.where(vote: true).size
-#   end
+  def upvotes
+    self.votes.where(vote: true).size
+  end
 
-#   def downvotes
-#     self.votes.where(vote: false).size
-#   end
+  def downvotes
+    self.votes.where(vote: false).size
+  end
 
-#   def vote_tally
-#     upvotes - downvotes
-#   end
-# end
+  def vote_tally
+    upvotes - downvotes
+  end
+end
